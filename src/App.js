c7afcab76675d69e538b05f26341efd9615888c8
takaki.otake <takaki.otake@PC-NO584.local>
2021-04-20 16:54:19 +0900

    add some modifications

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oh....Oh....
+          Oh....Oh....(feat)
         </p>
         <a
           className="App-link"
